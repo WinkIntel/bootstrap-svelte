9e0446a7eeaed41a609da5d2c1f88701e0f04f43
vince-winkintel <54216882+vince-winkintel@users.noreply.github.com>
2026-06-09 17:32:18 -0500

    Use Vercel adapter for showcase deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import sass from 'sass';
 import { sveltePreprocess } from 'svelte-preprocess'; // replace vitePreprocess with sveltePreprocess to use sass or sass-embedded
 
@@ -9,7 +9,7 @@ const config = {
         runes: true
     },
     kit: {
-        adapter: adapter()
+        adapter: adapter({ runtime: 'nodejs22.x' })
     },
     preprocess: sveltePreprocess({
         sourceMap: process.env.NODE_ENV === 'development',
